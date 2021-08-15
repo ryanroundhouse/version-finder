fe628d48ed8c60677005979c2252ec0d2f05b5ad
Ryan Graham <rg@ryangraham.ca>
2021-08-14 20:52:57 -0400

    added a supported-products endpoint that parses adependencies and looks for 'em

--- apps/version-finder-back/src/main.ts
+++ apps/version-finder-back/src/main.ts
@@ -1,8 +1,4 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { Dependency, Family } from '@version-finder/version-finder-lib';
 import * as express from 'express';
 import { VersionFinderApi } from './app/version-finder-api';
 
@@ -13,12 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to version-finder-back!' });
+  const deps: Dependency[] = [];
+  deps.push(new Dependency(1, new Family(), '1.0', true, []));
+  res.send(JSON.stringify(deps));
 });
+
+// [{"id":1,"family":{"id":0.5835383464402961},"version":"1.0","supported":true,"dependencies":[]}]
 
 app.post('/supported-products', (req, res) => {
   console.log(req.body);
-  res.send(versionFinderApi.whatProductsCanIRunWithDependency([]));
+  const dependencies: Dependency[] = JSON.parse(req.body.dependencies);
+  res.send(versionFinderApi.whatProductsCanIRunWithDependency(dependencies));
 });
 
 const port = process.env.port || 3333;
